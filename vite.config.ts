0fc61146bcd0fef6564e4c4428de05aa3abea18c
Pashafd <bayntis@gmail.com>
2023-04-22 00:35:32 +0300

    +config build

--- vite.config.ts
+++ vite.config.ts
@@ -24,20 +24,11 @@ export default defineConfig({
     ],
 
     // Uncomment this if you are using workers.
-    // worker: {
-    //  plugins: [
-    //    viteTsConfigPaths({
-    //      root: './',
-    //    }),
-    //  ],
-    // },
-
-    test: {
-        globals: true,
-        cache: {
-            dir: "./node_modules/.vitest",
-        },
-        environment: "jsdom",
-        include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    worker: {
+        plugins: [
+            viteTsConfigPaths({
+                root: "./",
+            }),
+        ],
     },
 });
