633463c54429ba0042c6ab638b6081d8627dfa3f
Pashafd <bayntis@gmail.com>
2023-04-24 19:34:06 +0300

    +cursor pointer for 3d sphere

--- src/components/3d/scroll-button.tsx
+++ src/components/3d/scroll-button.tsx
@@ -1,6 +1,7 @@
-import { useFrame } from "@react-three/fiber";
-import React, { useRef } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import React, { useEffect, useRef, useState } from "react";
 import { vhToPixels } from "src/components/scroll-to-buttons/scroll-to-bottom";
+import * as THREE from "three";
 
 const ScrollButton3d = (props: any) => {
     const mesh = useRef<any>();
@@ -17,6 +18,39 @@ const ScrollButton3d = (props: any) => {
     //     mesh.current.camera.position.z =
     //         mesh.current.camera.position.z * constant;
     // }
+    // Use useThree hook to get camera raycaster
+    const { camera } = useThree();
+    const raycaster = new THREE.Raycaster();
+
+    // Define mouse coordinates
+    const [mouse, setMouse] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+        const onMouseMove = (event: MouseEvent) => {
+            setMouse({
+                x: (event.clientX / window.innerWidth) * 2 - 1,
+                y: -(event.clientY / window.innerHeight) * 2 + 1,
+            });
+        };
+
+        window.addEventListener("mousemove", onMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", onMouseMove);
+        };
+    }, []);
+
+    // Add event listener to check for mouse hover on object
+    useFrame(() => {
+        const mouseVector = new THREE.Vector2(mouse.x, mouse.y);
+        raycaster.setFromCamera(mouseVector, camera);
+        const intersects = raycaster.intersectObjects([mesh.current]);
+        if (intersects.length > 0) {
+            document.body.style.cursor = "pointer";
+        } else {
+            document.body.style.cursor = "auto";
+        }
+    });
 
     return (
         <mesh
